19a218e6220142303fa6d7ec6bb0057929b40f66
togo-mentor <uint2435@gmail.com>
2022-01-16 07:54:15 +0900

    loading script tag

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,14 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          // 直接scriptを書く
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h2>
         <Link href="/">
           <a>Back to home</a>
